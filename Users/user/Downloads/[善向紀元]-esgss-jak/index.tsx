628c8f5412289d04cc92e2d71e11cfa65fcbd712
ESGss Developer <dev@example.com>
2026-01-04 15:10:38 +0800

    feat: Inject ESGss Genesis Seed Data

--- Users/user/Downloads/[善向紀元]-esgss-jak/index.tsx
+++ Users/user/Downloads/[善向紀元]-esgss-jak/index.tsx
@@ -7,11 +7,15 @@ import { UniversalAgentProvider } from './contexts/UniversalAgentContext';
 import { CompanyProvider } from './components/providers/CompanyProvider';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { injectGenesisData } from './utils/seed';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
+
+// 注入 ESGss 創世種子數據
+injectGenesisData();
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
